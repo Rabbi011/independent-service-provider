d6de4483921e467eb9459de66a84207a68b6d71d
RAKIBUL HASAN <rakibul15-13011@diu.edu.bd>
2022-04-18 05:39:35 +0600

    login and signup toglol done and login or signup succens then thore home pase

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,18 +1,31 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate()
 
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    if (user) {
+        navigate('/home');
+    }
+
     const handelSubmit = event => {
 
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(email, password)
+        // console.log(email, password)
+        signInWithEmailAndPassword(email, password);
 
 
     }
@@ -27,7 +40,7 @@ const Login = () => {
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-                   
+
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -1,8 +1,17 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const SignUp = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
     const navigate = useNavigate();
     const nameRef = useRef('');
     const emailRef = useRef('');
@@ -11,12 +20,16 @@ const SignUp = () => {
     const navigateLogin = () => {
         navigate('/login');
     }
+    if(user){
+        navigate('/home')
+    }
     const handelSignUp = event => {
         event.preventDefault();
         const name = nameRef.current.value;
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        
+        createUserWithEmailAndPassword(email, password)
+       
 
     }
 
